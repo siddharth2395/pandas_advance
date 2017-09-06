6579d8db0ede3654c6c05bfee63f23d912e8755f
siddharth bhavsar <siddharth2395@gmail.com>
2017-09-06 10:38:19 +0530

    dataframe from reading url

---
+++ dataframe_read_from_url.py
@@ -0,0 +1,6 @@
+import pandas as pd
+url = 'https://raw.github.com/gjreda/best-sandwiches/master/data/best-sandwiches-geocode.tsv'
+
+# fetch the text from the URL and read it into a DataFrame
+from_url = pd.read_table(url, sep='\t')
+print from_url.head(5)
